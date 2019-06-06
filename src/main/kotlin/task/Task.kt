55c91dcf491536edcbd7b6e9cb90d105ba3c0bb0
yohom <382146139@qq.com>
2019-06-06 15:19:23 +0800

    enhancement: 任务抽象类增加日志打印

--- src/main/kotlin/task/Task.kt
+++ src/main/kotlin/task/Task.kt
@@ -1,5 +1,11 @@
 package task
 
 abstract class Task<T, R>(input: T) {
+    init {
+        println("*******************************************************************************")
+        println("任务: ${this::class.simpleName} \n输入: $input")
+        println("*******************************************************************************")
+    }
+
     abstract fun process(): R
 }
