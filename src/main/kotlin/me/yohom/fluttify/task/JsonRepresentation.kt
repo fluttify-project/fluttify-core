7814dc024a56e02661670b53f02b44588382672d
yohom <382146139@qq.com>
2019-07-21 21:05:21 +0800

    chore: 调整

---
+++ src/main/kotlin/me/yohom/fluttify/task/JsonRepresentation.kt
@@ -0,0 +1,11 @@
+package me.yohom.fluttify.task
+
+import org.gradle.api.DefaultTask
+import org.gradle.api.tasks.TaskAction
+
+class JsonRepresentation : DefaultTask() {
+    @TaskAction
+    fun process() {
+
+    }
+}
