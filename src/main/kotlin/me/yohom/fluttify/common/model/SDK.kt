c1f6997ec2800fe73211aabc3a43c76952dfc68c
yohom <382146139@qq.com>
2019-07-23 14:02:27 +0800

    enhancement: 一个sdk内允许多个lib

--- src/main/kotlin/me/yohom/fluttify/common/model/SDK.kt
+++ src/main/kotlin/me/yohom/fluttify/common/model/SDK.kt
@@ -4,17 +4,24 @@ class SDK {
     /**
      * 版本
      */
-    var version: String = ""
-
-    /**
-     * 名称
-     */
-    var name: String = ""
+    var version: String = "0.0.1"
 
     /**
      * 平台
      */
     var platform: Platform = Platform.Unknown
+
+    /**
+     * 库 Android上是jar, iOS上是framework
+     */
+    val libs: MutableList<Lib> = mutableListOf()
+}
+
+class Lib {
+    /**
+     * 名称
+     */
+    var name: String = ""
 
     /**
      * 类
--- src/main/kotlin/me/yohom/fluttify/task/JsonRepresentation.kt
+++ src/main/kotlin/me/yohom/fluttify/task/JsonRepresentation.kt
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.databind.ObjectMapper
 import me.yohom.fluttify.common.extensions.file
 import me.yohom.fluttify.common.extensions.iterate
 import me.yohom.fluttify.common.extensions.javaType
+import me.yohom.fluttify.common.model.Lib
 import me.yohom.fluttify.common.model.Platform
 import me.yohom.fluttify.common.model.SDK
 import org.gradle.api.DefaultTask
@@ -14,27 +15,49 @@ import org.gradle.api.tasks.TaskAction
 open class AndroidJsonRepresentation : DefaultTask() {
     private val sdk = SDK()
 
-    @InputDirectory
-    var sdkDir = "${project.projectDir}/sdk/android/".file()
+    override fun getGroup() = "fluttify"
+
+    @TaskAction
+    fun process() {
+        val jsonFile = "${project.projectDir}/ir/android/json_representation.json".file()
+
+        sdk.platform = Platform.Android
+
+        "${project.buildDir}/decompiled/".file()
+            .listFiles()
+            ?.forEach {
+                val lib = Lib().apply { name = it.nameWithoutExtension }
+                it.iterate("java") { javaFile ->
+                    lib.types.add(javaFile.javaType())
+                }
+                sdk.libs.add(lib)
+            }
+        ObjectMapper().writerWithDefaultPrettyPrinter().writeValue(jsonFile, sdk)
+    }
+}
+
+open class IOSJsonRepresentation : DefaultTask() {
+    private val sdk = SDK()
 
     @InputDirectory
-    var sdkSourceDir = "${project.buildDir}/decompiled/".file()
+    var sdkSourceDir = "${project.projectDir}/sdk/ios/".file()
 
     @OutputFile
-    var jsonFile = "${project.projectDir}/ir/android/json_representation.json".file()
+    var jsonFile = "${project.projectDir}/ir/ios/json_representation.json".file()
 
     override fun getGroup() = "fluttify"
 
     @TaskAction
     fun process() {
-        // 获取sdk的名称 目前假设sdk中只存在一个jar
-        sdk.name = sdkDir.list()?.firstOrNull { it.endsWith(".jar") } ?: throw IllegalArgumentException("没有jar")
-        sdk.platform = Platform.Android
-        sdkSourceDir.iterate("java") {
-            sdk.types.add(it.javaType())
-        }
-
-        ObjectMapper().writerWithDefaultPrettyPrinter().writeValue(jsonFile, sdk)
+//        val sdkDir = project.extensions.getByType(FluttifyExtension::class.java).jarFile.file()
+//        // 获取sdk的名称 目前假设sdk中只存在一个jar
+//        sdk.name = sdkDir.list()?.firstOrNull { it.endsWith(".jar") } ?: throw IllegalArgumentException("没有jar")
+//        sdk.platform = Platform.Android
+//        sdkSourceDir.iterate("java") {
+//            sdk.types.add(it.javaType())
+//        }
+//
+//        ObjectMapper().writerWithDefaultPrettyPrinter().writeValue(jsonFile, sdk)
     }
 
 }
