f68024099d17bf345f55a20648f8bceb308fa621
yohom <382146139@qq.com>
2019-08-29 13:46:22 +0800

    enhancement: ios: 创建好PlatformView之后加入REF_MAP

--- src/main/kotlin/me/yohom/fluttify/common/tmpl/objc/plugin/handler/ArgEnumTmpl.kt
+++ src/main/kotlin/me/yohom/fluttify/common/tmpl/objc/plugin/handler/ArgEnumTmpl.kt
@@ -3,7 +3,7 @@ package me.yohom.fluttify.common.tmpl.objc.plugin.handler
 import me.yohom.fluttify.common.model.Variable
 
 //// 枚举参数
-//#__type_name__# #__arg_name__# = (#__type_name__#) args[@"#__arg_name__#"];
+//#__type_name__# #__arg_name__# = (#__type_name__#) [args[@"#__arg_name__#"] integerValue];
 internal class ArgEnumTmpl(private val variable: Variable) {
     private val tmpl = this::class.java.getResource("/tmpl/objc/arg_enum.stmt.m.tmpl").readText()
 
--- src/main/kotlin/me/yohom/fluttify/common/tmpl/objc/plugin/handler/ArgRefTmpl.kt
+++ src/main/kotlin/me/yohom/fluttify/common/tmpl/objc/plugin/handler/ArgRefTmpl.kt
@@ -3,9 +3,9 @@ package me.yohom.fluttify.common.tmpl.objc.plugin.handler
 import me.yohom.fluttify.common.model.Variable
 
 //// 引用参数
-//val #__arg_name__# = REF_MAP[args["#__arg_name__#"] as Int] as #__type_name__#
+//let #__arg_name__# = REF_MAP[args["#__arg_name__#"] as Int] as! #__type_name__#
 internal class ArgRefTmpl(private val variable: Variable) {
-    private val tmpl = this::class.java.getResource("/tmpl/swift/arg_ref.stmt.swift.tmpl").readText()
+    private val tmpl = this::class.java.getResource("/tmpl/objc/arg_ref.stmt.m.tmpl").readText()
 
     fun objcArgRef(): String {
         return tmpl
