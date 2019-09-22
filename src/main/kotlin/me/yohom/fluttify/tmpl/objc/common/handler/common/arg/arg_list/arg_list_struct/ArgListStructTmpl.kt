a61a75349d08a9e96e38e7cc4a552494b3fedc43
yohom <382146139@qq.com>
2019-09-22 21:33:45 +0800

    bugfix: 添加少的##

--- src/main/kotlin/me/yohom/fluttify/tmpl/objc/common/handler/common/arg/arg_list/arg_list_struct/ArgListStructTmpl.kt
+++ src/main/kotlin/me/yohom/fluttify/tmpl/objc/common/handler/common/arg/arg_list/arg_list_struct/ArgListStructTmpl.kt
@@ -4,14 +4,14 @@ import me.yohom.fluttify.extensions.depointer
 import me.yohom.fluttify.model.Variable
 
 //// 列表参数
-//NSArray* #__arg_name__#RefIdArray = (NSArray*) args[@"__arg_name__"];
-//#__type_name__# #__arg_name__#Array[#__arg_name__#RefIdArray.count];
+//NSArray* #__arg_name__#RefIdArray = (NSArray*) args[@"#__arg_name__#"];
+//#__type_name__# #__arg_name__#[#__arg_name__#RefIdArray.count];
 //
 //for (int i = 0; i < #__arg_name__#RefIdArray.count; i++) {
 //    NSValue* #__arg_name__#Value = (NSValue*) REF_MAP[[#__arg_name__#RefIdArray objectAtIndex:i]];
-//    #__type_name__# #__arg_name__#;
-//    [#__arg_name__#Value getValue:&#__arg_name__#];
-//    #__arg_name__#Array[i] = #__arg_name__#;
+//    #__type_name__# #__arg_name__#Item;
+//    [#__arg_name__#Value getValue:&#__arg_name__#Item];
+//    #__arg_name__#[i] = #__arg_name__#Item;
 //}
 internal class ArgListStructTmpl(private val variable: Variable) {
     private val tmpl = this::class.java.getResource("/tmpl/objc/arg_list_struct.stmt.m.tmpl").readText()
