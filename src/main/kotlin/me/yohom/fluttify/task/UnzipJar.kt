8e29a9d86ab7c6c432929dfb49c04042db042d0a
yohom <382146139@qq.com>
2019-10-10 22:02:42 +0800

    enhancement: unzip任务实现InputFile和OutputFile

--- src/main/kotlin/me/yohom/fluttify/task/UnzipJar.kt
+++ src/main/kotlin/me/yohom/fluttify/task/UnzipJar.kt
@@ -1,7 +1,8 @@
 package me.yohom.fluttify.task
 
-import me.yohom.fluttify.FluttifyExtension
 import me.yohom.fluttify.extensions.file
+import org.gradle.api.tasks.InputFile
+import org.gradle.api.tasks.OutputFile
 import org.gradle.api.tasks.TaskAction
 import java.io.BufferedReader
 import java.io.File
@@ -9,20 +10,23 @@ import java.io.InputStreamReader
 
 /**
  * 解压缩Jar任务
- *
- * 输入: 反编译后的Jar文件
- * 输出: class文件列表
- * 依赖: []
  */
 open class UnzipJar : FluttifyTask() {
+
+    @InputFile
+    var jarFile: File? = null
+
+    @OutputFile
+    var unzippedJar: File? = null
+
     @TaskAction
     fun unzip() {
-        val ext = project.extensions.getByType(FluttifyExtension::class.java)
-        val jarPath = ext.jarDir.file().listFiles()?.first { it.name.endsWith("jar") }?.absolutePath
+        jarFile = ext.jarDir.file().listFiles()?.first { it.name.endsWith("jar") }
+        unzippedJar = "${ext.jarDir}unzip/".file()
 
-        val unzippedJar: File = "${ext.jarDir}unzip/".file()
+        if (jarFile == null || unzippedJar == null) return
 
-        val process = Runtime.getRuntime().exec("unzip -o $jarPath -d $unzippedJar")
+        val process = Runtime.getRuntime().exec("unzip -o ${jarFile?.absolutePath} -d $unzippedJar")
         val br = BufferedReader(InputStreamReader(process.inputStream))
         br.lines().forEach(::println)
     }
