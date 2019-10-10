d103aee2ab2589bb4ecc414df6a91c41d7e8ce60
yohom <382146139@qq.com>
2019-10-10 22:25:10 +0800

    enhancement: Export任务实现输入输出

--- src/main/kotlin/me/yohom/fluttify/task/Export.kt
+++ src/main/kotlin/me/yohom/fluttify/task/Export.kt
@@ -2,21 +2,33 @@ package me.yohom.fluttify.task
 
 import me.yohom.fluttify.extensions.file
 import me.yohom.fluttify.extensions.iterate
+import org.gradle.api.tasks.InputDirectory
+import org.gradle.api.tasks.OutputFiles
 import org.gradle.api.tasks.TaskAction
 
 /**
  * 需要引入的内容为android端的主类和模型类以及ios端的主类和模型类
  */
 open class Export : FluttifyTask() {
+    @InputDirectory
+    val dartSourceDir = listOf(
+        "${project.projectDir}/output-project/${ext.outputProjectName}/lib/src/android/".file(),
+        "${project.projectDir}/output-project/${ext.outputProjectName}/lib/src/ios/".file()
+    )
+
+    @OutputFiles
+    val exportFiles = listOf(
+        "${project.projectDir}/output-project/${ext.outputProjectName}/lib/src/android/android.export.dart".file(),
+        "${project.projectDir}/output-project/${ext.outputProjectName}/lib/src/ios/ios.export.dart".file()
+    )
+
     @TaskAction
     fun process() {
-        val androidDartSrcDir = "${project.projectDir}/output-project/${ext.outputProjectName}/lib/src/android/".file()
-        val iosDartSrcDir = "${project.projectDir}/output-project/${ext.outputProjectName}/lib/src/ios/".file()
+        val androidDartSrcDir = dartSourceDir[0]
+        val iosDartSrcDir = dartSourceDir[1]
 
-        val androidExportFile =
-            "${project.projectDir}/output-project/${ext.outputProjectName}/lib/src/android/android.export.dart".file()
-        val iosExportFile =
-            "${project.projectDir}/output-project/${ext.outputProjectName}/lib/src/ios/ios.export.dart".file()
+        val androidExportFile = exportFiles[0]
+        val iosExportFile = exportFiles[1]
 
         val androidResult = StringBuilder()
         val iosResult = StringBuilder()
