f04a80f01237d65e09b8c280a8637cdfcacb9d0e
yohom <382146139@qq.com>
2019-10-11 09:55:52 +0800

    chore: 整理

--- src/main/kotlin/me/yohom/fluttify/extensions/ClassContextX.kt
+++ src/main/kotlin/me/yohom/fluttify/extensions/ClassContextX.kt
@@ -105,18 +105,6 @@ fun JavaParser.InterfaceDeclarationContext.isPublic(): Boolean {
         ?.map { it.text }
         ?.contains("public") == true
 }
-
-/**
- * 是否static
- * todo 解决静态内部类和非静态内部类如何区别
- */
-fun JavaParser.InterfaceDeclarationContext.isStatic(): Boolean {
-//    return ancestorOf(JavaParser.TypeDeclarationContext::class)
-//        ?.classOrInterfaceModifier()
-//        ?.map { it.text }
-//        ?.contains("static") == true
-    return true
-}
 //endregion
 
 //region Objc类扩展
