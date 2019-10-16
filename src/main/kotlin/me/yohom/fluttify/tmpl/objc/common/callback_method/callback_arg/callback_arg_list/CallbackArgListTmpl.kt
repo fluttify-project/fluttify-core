c85f67a81f1a7e3d8b98b985eb4e46274ce46ff7
yohom <382146139@qq.com>
2019-10-16 15:02:49 +0800

    enhancement: 列表回调参数 修正

--- src/main/kotlin/me/yohom/fluttify/tmpl/objc/common/callback_method/callback_arg/callback_arg_list/CallbackArgListTmpl.kt
+++ src/main/kotlin/me/yohom/fluttify/tmpl/objc/common/callback_method/callback_arg/callback_arg_list/CallbackArgListTmpl.kt
@@ -3,11 +3,14 @@ package me.yohom.fluttify.tmpl.objc.common.callback_method.callback_arg.callback
 import me.yohom.fluttify.extensions.enpointer
 import me.yohom.fluttify.model.Parameter
 
-//// 列表参数
+//// 列表回调参数
 //NSMutableArray<NSNumber*>* arg#__arg_name__# = [NSMutableArray arrayWithCapacity:#__arg_name__#.count];
 //for (int i = 0; i < #__arg_name__#.count; i++) {
-//    arg#__arg_name__#[i] = @(#__arg_name__#.hash);
-//    HEAP[@(#__arg_name__#.hash)] = #__arg_name__#;
+//    NSObject* item = ((NSObject*) [#__arg_name__# objectAtIndex:i]);
+//    // 返回给dart端的数据
+//    arg#__arg_name__#[i] = @(item.hash);
+//    // 放到HEAP中的数据
+//    HEAP[@(item.hash)] = item;
 //}
 internal class CallbackArgListTmpl(private val param: Parameter) {
     private val tmpl = this::class.java.getResource("/tmpl/objc/callback_arg_list.stmt.m.tmpl").readText()
