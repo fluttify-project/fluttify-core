ef6713f41b699a333605fb3c5f8dd23f176d3fdc
yohom <382146139@qq.com>
2019-10-22 11:56:22 +0800

    feature: 通过反射调用方法

--- src/main/kotlin/me/yohom/fluttify/tmpl/dart/type/type_ref/TypeRefTmpl.kt
+++ src/main/kotlin/me/yohom/fluttify/tmpl/dart/type/type_ref/TypeRefTmpl.kt
@@ -9,14 +9,15 @@ import me.yohom.fluttify.tmpl.dart.type.type_ref.type_check.TypeCheckTmpl
 
 //import 'dart:typed_data';
 //
-//import 'package:#__current_package__#/#__current_package__#.dart';
+//import 'package:#__current_package__#/src/ios/ios.export.g.dart';
+//import 'package:#__current_package__#/src/android/android.export.g.dart';
 //import 'package:flutter/services.dart';
 //
-//// ignore_for_file: non_constant_identifier_names, camel_case_types
-//class Ref_#__platform__# {
-//  Ref(this.refId);
-//
-//  final int refId;
+//// ignore_for_file: non_constant_identifier_names, camel_case_types, missing_return, unused_import
+//class Ref_#__platform__# extends Ref {
+//  Future<void> performSelectorWithObject(String selector, Object object) {
+//    return MethodChannel('__method_channel__').invokeMethod('Ref_#__platform__#::performSelectorWithObject', {'refId': refId, 'selector': selector, 'object': object});
+//  }
 //
 //  #__type_checks__#
 //
@@ -33,6 +34,7 @@ class TypeRefTmpl(
         return tmpl
             .replace("#__current_package__#", currentPackage)
             .replace("#__platform__#", sdk.platform.toString())
+            .replace("#__method_channel__#", ext.methodChannelName)
             .replaceParagraph(
                 "#__type_checks__#",
                 sdk.libs
