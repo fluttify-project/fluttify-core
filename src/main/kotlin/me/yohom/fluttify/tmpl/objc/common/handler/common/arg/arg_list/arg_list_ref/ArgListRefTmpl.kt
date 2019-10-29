3b3c13bf50eb1986764e691779b2da4be32cce30
yohom <382146139@qq.com>
2019-10-29 20:46:01 +0800

    fix: objc: 循环取值时, size去错了

--- src/main/kotlin/me/yohom/fluttify/tmpl/objc/common/handler/common/arg/arg_list/arg_list_ref/ArgListRefTmpl.kt
+++ src/main/kotlin/me/yohom/fluttify/tmpl/objc/common/handler/common/arg/arg_list/arg_list_ref/ArgListRefTmpl.kt
@@ -1,16 +1,14 @@
 package me.yohom.fluttify.tmpl.objc.common.handler.common.arg.arg_list.arg_list_ref
 
-import me.yohom.fluttify.ext
 import me.yohom.fluttify.extensions.depointer
 import me.yohom.fluttify.extensions.enpointer
 import me.yohom.fluttify.extensions.enprotocol
-import me.yohom.fluttify.extensions.underscore2Camel
 import me.yohom.fluttify.model.Variable
 
-//// 列表参数
+//// list arg
 //NSArray<NSNumber*>* #__arg_name__#RefArray = (NSArray<NSNumber*> *) args[@"#__arg_name__#"];
 //NSMutableArray<#__type_name__#>* #__arg_name__# = [NSMutableArray arrayWithCapacity:#__arg_name__#RefArray.count];
-//for (int i = 0; i < #__arg_name__#.count; i++) {
+//for (int i = 0; i < #__arg_name__#RefArray.count; i++) {
 //    #__type_name__# item = (#__type_name__#) HEAP[[#__arg_name__#RefArray objectAtIndex:i]];
 //    [#__arg_name__# addObject:item];
 //}
