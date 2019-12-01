7434fc167cd1d467c732363245174ff39a91425d
yohom <382146139@qq.com>
2019-12-01 21:32:06 +0800

    fix: [objc] 回调方法的参数 NSString指针

--- src/main/kotlin/me/yohom/fluttify/tmpl/objc/common/callback/common/callback_arg/callback_arg_jsonable/CallbackArgJsonableTmpl.kt
+++ src/main/kotlin/me/yohom/fluttify/tmpl/objc/common/callback/common/callback_arg/callback_arg_jsonable/CallbackArgJsonableTmpl.kt
@@ -1,6 +1,8 @@
 package me.yohom.fluttify.tmpl.objc.common.callback.common.callback_arg.callback_arg_jsonable
 
 import me.yohom.fluttify.extensions.depointer
+import me.yohom.fluttify.extensions.enpointer
+import me.yohom.fluttify.extensions.isValueType
 import me.yohom.fluttify.model.Variable
 
 //// jsonable回调参数
@@ -10,7 +12,9 @@ internal class CallbackArgJsonableTmpl(private val variable: Variable) {
 
     fun objcCallbackArgJsonable(): String {
         return tmpl
-            .replace("#__type_name__#", variable.typeName)
+            .replace("#__type_name__#", variable.typeName.run {
+                if (this.isValueType()) this else this.enpointer()
+            })
             .replace("#__arg_name__#", variable.name.depointer())
     }
 }
