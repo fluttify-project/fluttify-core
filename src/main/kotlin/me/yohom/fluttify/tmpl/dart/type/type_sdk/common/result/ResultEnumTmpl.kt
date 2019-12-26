8ed5530ae65308f98686836c78bb15c288921a6b
yohom <yohombao@gmail.com>
2019-12-26 10:47:17 +0800

    enhance: 返回枚举列表的方法处理

--- src/main/kotlin/me/yohom/fluttify/tmpl/dart/type/type_sdk/common/result/ResultEnumTmpl.kt
+++ src/main/kotlin/me/yohom/fluttify/tmpl/dart/type/type_sdk/common/result/ResultEnumTmpl.kt
@@ -1,6 +1,8 @@
 package me.yohom.fluttify.tmpl.dart.type.type_sdk.common.result
 
 import me.yohom.fluttify.TYPE_NAME
+import me.yohom.fluttify.extensions.genericType
+import me.yohom.fluttify.extensions.isCollection
 import me.yohom.fluttify.extensions.toDartType
 
 //#__type_name__#.values[result]
@@ -8,7 +10,11 @@ class ResultEnumTmpl(private val returnType: TYPE_NAME) {
     private val tmpl = this::class.java.getResource("/tmpl/dart/result_enum.stmt.dart.tmpl").readText()
 
     fun dartResultEnum(): String {
-        return tmpl
-            .replace("#__type_name__#", returnType.toDartType())
+        val enumResult = tmpl.replace("#__type_name__#", returnType.toDartType().genericType())
+        return if (returnType.isCollection()) {
+            "(result as List).cast<int>().map((result) => $enumResult)"
+        } else {
+            enumResult
+        }
     }
 }
