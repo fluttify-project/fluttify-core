2dbed18c22f51246391fa58151e690c168661a0d
yohom <yohombao@gmail.com>
2020-01-26 21:40:52 +0800

    feat: 私有仓库搭建完成

--- src/main/kotlin/me/yohom/fluttify/task/FluttifyTask.kt
+++ src/main/kotlin/me/yohom/fluttify/task/FluttifyTask.kt
@@ -2,13 +2,10 @@ package me.yohom.fluttify.task
 
 import me.yohom.fluttify.FluttifyExtension
 import org.gradle.api.DefaultTask
-import org.gradle.api.tasks.Input
 
 abstract class FluttifyTask : DefaultTask() {
-    @get:Input
     val ext: FluttifyExtension
         get() = project.extensions.getByType(FluttifyExtension::class.java)
 
-    @Input
     override fun getGroup() = "fluttify"
 }
