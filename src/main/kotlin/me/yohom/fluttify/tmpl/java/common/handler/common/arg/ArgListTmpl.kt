f8c172967e12b3c167887e472b5bf3ce98b65ad3
yohom <yohombao@qq.com>
2020-02-20 19:29:05 +0800

    fix: List参数处理修复

--- src/main/kotlin/me/yohom/fluttify/tmpl/java/common/handler/common/arg/ArgListTmpl.kt
+++ src/main/kotlin/me/yohom/fluttify/tmpl/java/common/handler/common/arg/ArgListTmpl.kt
@@ -5,15 +5,16 @@ import me.yohom.fluttify.extensions.getResource
 import me.yohom.fluttify.model.Variable
 
 //// list arg
-//List<Integer> #__arg_name__#RefIdList = (List<Integer>) args.get("#__arg_name__#");
+//List<Integer> #__arg_name__#RefIdList = (List<Integer>) ((Map<String, Object>) args).get("#__arg_name__#");
 //List<#__type_name__#> #__arg_name__# = new ArrayList<>();
-//for (int refId : refIdList) {
-//    #__arg_name__#.add((#__type_name__#) getHEAP().get(refIdList.get(refId)));
+//for (int refId : #__arg_name__#RefIdList) {
+//    #__arg_name__#.add((#__type_name__#) getHEAP().get(refId));
 //}
 private val tmpl = getResource("/tmpl/java/arg_list.stmt.java.tmpl").readText()
 
 fun ArgListTmpl(variable: Variable): String {
-    return if (variable.genericLevel == 0) {
+    // 只处理非列表和一维列表, 多维列表一律返回一个空的列表
+    return if (variable.genericLevel <= 1) {
         tmpl
             .replace("#__type_name__#", variable.typeName.replace("$", "."))
             .replace("#__arg_name__#", variable.name)
