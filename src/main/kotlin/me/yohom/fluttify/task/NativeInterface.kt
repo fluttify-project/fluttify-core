e53b7501ef032abc2e34aa09f21ba449a3f32e3d
yohom <yohombao@qq.com>
2020-03-04 20:32:13 +0800

    enhance: objc: 拆分handler完成

--- src/main/kotlin/me/yohom/fluttify/task/NativeInterface.kt
+++ src/main/kotlin/me/yohom/fluttify/task/NativeInterface.kt
@@ -11,11 +11,18 @@ import me.yohom.fluttify.tmpl.java.common.handler.handler_object_creator_batch.H
 import me.yohom.fluttify.tmpl.java.common.handler.handler_setter.HandlerSetterTmpl
 import me.yohom.fluttify.tmpl.java.common.handler.handler_setter_batch.HandlerSetterBatchTmpl
 import me.yohom.fluttify.tmpl.java.plugin.JavaPluginTmpl
-import me.yohom.fluttify.tmpl.java.plugin.sub_handler.SubHandlerTmpl
+import me.yohom.fluttify.tmpl.objc.common.handler.handler_object_creator.handler_object_creator_ref.HandlerObjectFactoryRefTmpl
+import me.yohom.fluttify.tmpl.objc.common.handler.handler_object_creator.handler_object_creator_ref_batch.HandlerObjectFactoryRefBatchTmpl
+import me.yohom.fluttify.tmpl.objc.common.handler.handler_object_creator.handler_object_creator_struct.HandlerObjectFactoryStructTmpl
+import me.yohom.fluttify.tmpl.objc.common.handler.handler_object_creator.handler_object_creator_struct_batch.HandlerObjectFactoryStructBatchTmpl
+import me.yohom.fluttify.tmpl.objc.common.handler.handler_type_cast.HandlerTypeCastTmpl
+import me.yohom.fluttify.tmpl.objc.common.handler.handler_type_check.HandlerTypeCheckTmpl
 import me.yohom.fluttify.tmpl.objc.plugin.ObjcPluginTmpl
 import org.gradle.api.tasks.TaskAction
 import me.yohom.fluttify.tmpl.java.platform_view_factory.PlatformViewFactoryTmpl as JavaPlatformViewFactory
+import me.yohom.fluttify.tmpl.java.plugin.sub_handler.SubHandlerTmpl as JavaSubHandlerTmpl
 import me.yohom.fluttify.tmpl.objc.platform_view_factory.PlatformViewFactoryTmpl as ObjcPlatformViewFactory
+import me.yohom.fluttify.tmpl.objc.plugin.sub_handler.SubHandlerTmpl as ObjcSubHandlerTmpl
 
 /**
  * Android端接口生成
@@ -101,7 +108,7 @@ open class AndroidJavaInterface : FluttifyTask() {
                 .toObservable()
                 .buffer(200)
                 .blockingIterable()
-                .mapIndexed { index, subHandler -> SubHandlerTmpl(index, subHandler) }
+                .mapIndexed { index, subHandler -> JavaSubHandlerTmpl(index, subHandler) }
                 .forEachIndexed { index, content ->
                     subHandlerOutputFile.replace("#__number__#", index.toString()).file().writeText(content)
                 }
@@ -143,18 +150,129 @@ open class IOSObjcInterface : FluttifyTask() {
         val projectRootDir = "${project.projectDir}/output-project/${ext.projectName}/ios/Classes/"
         val pluginHFile = "$projectRootDir/${ext.projectName.underscore2Camel()}Plugin.h"
         val pluginMFile = "$projectRootDir/${ext.projectName.underscore2Camel()}Plugin.m"
+        val subHandlerOutputDir = "$projectRootDir/SubHandler/"
+        val subHandlerOutputHFile = "$subHandlerOutputDir/SubHandler#__number__#.h"
+        val subHandlerOutputMFile = "$subHandlerOutputDir/SubHandler#__number__#.m"
 
         val sdk = jrFile.readText().fromJson<SDK>()
 
         // 生成前先删除之前的文件
         if (sdk.directLibs.isNotEmpty()) projectRootDir.file().deleteRecursively()
 
-        // 生成主plugin文件
-        ObjcPluginTmpl(sdk.directLibs)
-            .run {
-                pluginHFile.file().writeText(this[0])
-                pluginMFile.file().writeText(this[1])
-            }
+        sdk.directLibs.forEach { lib ->
+            val getters = lib.types
+                .filterType()
+                .flatMap { it.fields }
+                .filterGetters()
+                .map { me.yohom.fluttify.tmpl.objc.common.handler.handler_getter.HandlerGetterTmpl(it) }
+
+            val gettersBatch = lib.types
+                .filterType()
+                .flatMap { it.fields }
+                .filterGetters()
+                .map { me.yohom.fluttify.tmpl.objc.common.handler.handler_getter_batch.HandlerGetterBatchTmpl(it) }
+
+            val setters = lib.types
+                .filterType()
+                .flatMap { it.fields }
+                .filterSetters()
+                .map { me.yohom.fluttify.tmpl.objc.common.handler.handler_setter.HandlerSetterTmpl(it) }
+
+            val settersBatch = lib.types
+                .filterType()
+                .flatMap { it.fields }
+                .filterSetters(true)
+                .map { me.yohom.fluttify.tmpl.objc.common.handler.handler_setter_batch.HandlerSetterBatchTmpl(it) }
+
+            val functions = lib.types
+                // 暂时先不处理含有lambda的函数
+                .filter { it.isKnownFunction() && it.formalParams.all { !it.variable.isLambda() } }
+                .map { it.asMethod() }
+                .map { me.yohom.fluttify.tmpl.objc.common.handler.handler_method.HandlerMethodTmpl(it) }
+
+            val methods = lib.types
+                .filterType()
+                .flatMap { it.methods }
+                .filterMethod()
+                .map { me.yohom.fluttify.tmpl.objc.common.handler.handler_method.HandlerMethodTmpl(it) }
+
+            val methodsBatch = lib.types
+                .filterType()
+                .flatMap { it.methods }
+                .filterMethod(true)
+                .map { me.yohom.fluttify.tmpl.objc.common.handler.handler_method_batch.HandlerMethodBatchTmpl(it) }
+
+            val typeCasts = lib.types
+                .filterType()
+                .asSequence()
+                .filterNot { it.isLambda() }
+                .filterNot { it.isFunction() }
+                .filterNot { it.isAlias() }
+                .distinctBy { it.name }
+                .filter { !it.isInterface() && !it.isEnum() && !it.isStruct() }
+                .map { HandlerTypeCastTmpl(it) }
+                .toList()
+
+            val typeChecks = lib.types
+                .filterType()
+                .asSequence()
+                .filterNot { it.isLambda() }
+                .filterNot { it.isFunction() }
+                .filterNot { it.isAlias() }
+                .distinctBy { it.name }
+                .filter { !it.isInterface() && !it.isEnum() && !it.isStruct() }
+                .map { HandlerTypeCheckTmpl(it) }
+                .toList()
+
+            val objectCreators = lib.types
+                .filterConstructable()
+                .distinctBy { it.name }
+                .map {
+                    if (it.isStruct()) {
+                        HandlerObjectFactoryStructTmpl(it)
+                    } else {
+                        HandlerObjectFactoryRefTmpl(it)
+                    }
+                }
+
+            val objectCreatorsBatch = lib.types
+                .filterConstructable()
+                .distinctBy { it.name }
+                .map {
+                    if (it.isStruct()) {
+                        HandlerObjectFactoryStructBatchTmpl(it)
+                    } else {
+                        HandlerObjectFactoryRefBatchTmpl(it)
+                    }
+                }
+
+            methods
+                .union(methodsBatch)
+                .union(getters)
+                .union(gettersBatch)
+                .union(setters)
+                .union(settersBatch)
+                .union(typeChecks)
+                .union(typeCasts)
+                .union(objectCreators)
+                .union(objectCreatorsBatch)
+                .union(functions)
+                .toObservable()
+                .buffer(200)
+                .blockingIterable()
+                .mapIndexed { index, subHandler -> ObjcSubHandlerTmpl(index, subHandler) }
+                .forEachIndexed { index, content ->
+                    subHandlerOutputHFile.replace("#__number__#", index.toString()).file().writeText(content[0])
+                    subHandlerOutputMFile.replace("#__number__#", index.toString()).file().writeText(content[1])
+                }
+
+            // 生成主plugin文件
+            ObjcPluginTmpl(sdk.directLibs, subHandlerOutputDir)
+                .run {
+                    pluginHFile.file().writeText(this[0])
+                    pluginMFile.file().writeText(this[1])
+                }
+        }
 
         // 生成PlatformViewFactory文件
         sdk.directLibs
---
+++ src/main/kotlin/me/yohom/fluttify/tmpl/objc/plugin/register_handler/RegisterHandlerTmpl.kt
@@ -0,0 +1,12 @@
+package me.yohom.fluttify.tmpl.objc.plugin.register_handler
+
+import me.yohom.fluttify.extensions.getResource
+
+//[_handlerMap addEntriesFromDictionary: [self getSubHandler#__number__#]];
+private val tmpl = getResource("/tmpl/objc/register_handler.stmt.m.tmpl").readText()
+
+fun RegisterHandlerTmpl(number: Int): String {
+    val numberString = number.toString()
+
+    return tmpl.replace("#__number__#", numberString)
+}
