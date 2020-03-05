9bc6e29d2da7ad0a358a15673796450c90e91e44
yohom <yohombao@qq.com>
2020-03-05 15:43:20 +0800

    enhance: java: long类型的参数一律转成int处理

--- src/main/kotlin/me/yohom/fluttify/tmpl/java/common/handler/common/arg/ArgJsonableTmpl.kt
+++ src/main/kotlin/me/yohom/fluttify/tmpl/java/common/handler/common/arg/ArgJsonableTmpl.kt
@@ -11,7 +11,17 @@ import me.yohom.fluttify.model.Variable
 private val tmpl = getResource("/tmpl/java/arg_jsonable.stmt.java.tmpl").readText()
 
 fun ArgJsonableTmpl(variable: Variable): String {
-    val type = variable.typeName.run { if (toLowerCase() == "float") "Double" else this }
+    val type = variable.typeName.run {
+        when {
+            // java端不会以float接收数据, 一律都是double
+            toLowerCase() == "float" -> "Double"
+            // java.lang.ClassCastException: java.lang.Integer cannot be cast to java.lang.
+            // dart端只有int, 不区分32位和64位, java端具体类型由数值大小决定, 所以如果要精确处理这个问题的话会比较麻烦, 这里先一律转成int处理
+            // 后期如果真的碰到int无法满足的情况再想办法
+            toLowerCase() == "long" -> "int"
+            else -> this
+        }
+    }
     return tmpl
         .replace(
             "#__type_name__#",
