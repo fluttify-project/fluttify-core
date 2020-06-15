0f94116f94574fa01da080db03f4855a9cd40c9a
yohom <382146139@qq.com>
2020-06-15 12:44:17 +0800

    enhance: java端处理null的情况

--- src/main/kotlin/me/yohom/fluttify/tmpl/java/common/handler/common/arg/ArgRefTmpl.kt
+++ src/main/kotlin/me/yohom/fluttify/tmpl/java/common/handler/common/arg/ArgRefTmpl.kt
@@ -4,8 +4,10 @@ import me.yohom.fluttify.extensions.getResource
 import me.yohom.fluttify.model.Variable
 
 //// ref arg
-//#__type_name__# #__arg_name__# = (#__type_name__#) HEAP.get((int) args.get("#__arg_name__#"));
+//Integer __refId__ = (Integer) ((Map<String, Object>) __args__).get("#__arg_name__#");
+//#__type_name__# #__arg_name__# = __refId__ != null ? (#__type_name__#) getHEAP().get(__refId__) : null;
 private val tmpl by lazy { getResource("/tmpl/java/arg_ref.stmt.java.tmpl").readText() }
+
 fun ArgRefTmpl(variable: Variable): String {
     return tmpl
         .replace("#__type_name__#", variable.trueType.replace("$", "."))
