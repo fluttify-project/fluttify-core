cbcb910ecd6a8106a40cdea83aeae7a6328ab93b
yohom <382146139@qq.com>
2020-06-19 22:16:07 +0800

    enhance: 调整模板

--- src/main/kotlin/me/yohom/fluttify/Global.kt
+++ src/main/kotlin/me/yohom/fluttify/Global.kt
@@ -11,7 +11,7 @@ var CONSTRUCTOR_LOG = false
 var VARIABLE_LOG = false
 var FIELD_LOG = false
 var TYPE_LOG = true
-var METHOD_LOG = false
+var METHOD_LOG = true
 
 private var idSequence = 0
 val NEXT_ID
