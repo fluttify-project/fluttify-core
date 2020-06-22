60907761adcf60d30543d273d52e33d41558eb40
yohom <382146139@qq.com>
2020-06-22 08:57:21 +0800

    enhance: java回调channel名称修正

--- src/main/kotlin/me/yohom/fluttify/tmpl/java/common/handler/common/invoke/common/callback/CallbackTmpl.kt
+++ src/main/kotlin/me/yohom/fluttify/tmpl/java/common/handler/common/invoke/common/callback/CallbackTmpl.kt
@@ -4,13 +4,12 @@ import me.yohom.fluttify.extensions.getResource
 import me.yohom.fluttify.extensions.ifIsGenericTypeConvertToObject
 import me.yohom.fluttify.extensions.joinToStringX
 import me.yohom.fluttify.extensions.replaceParagraph
-import me.yohom.fluttify.model.Method
 import me.yohom.fluttify.model.Type
 import me.yohom.fluttify.tmpl.java.common.handler.common.invoke.common.callback.callback_method.CallbackMethodTmpl
 
 //new #__callback_class_name__#() {
 //        // method channel
-//        MethodChannel callbackChannel = new MethodChannel(messenger, "#__callback_channel__#::Callback");
+//        MethodChannel callbackChannel = new MethodChannel(messenger, "#__callback_channel__#");
 //        android.os.Handler handler = new android.os.Handler(android.os.Looper.getMainLooper());
 //
 //        // call dart method
@@ -18,13 +17,13 @@ import me.yohom.fluttify.tmpl.java.common.handler.common.invoke.common.callback.
 //}
 private val tmpl by lazy { getResource("/tmpl/java/callback.stmt.java.tmpl").readText() }
 
-fun CallbackTmpl(callerMethod: Method, callbackType: Type): String {
+fun CallbackTmpl(callbackType: Type): String {
     val containerType = callbackType.name.replace("$", ".")
     val genericTypes = callbackType.definedGenericTypes
         .map { it.ifIsGenericTypeConvertToObject() }
         .joinToStringX(prefix = "<", suffix = ">")
     val className = "${containerType}${genericTypes}"
-    val callbackChannel = callerMethod.nameWithClass()
+    val callbackChannel = "${callbackType.name}::Callback"
     val callbackMethods = callbackType.methods.joinToString("\n") { CallbackMethodTmpl(it) }
     return tmpl
         .replace("#__callback_class_name__#", className)
