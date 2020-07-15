c799c8a23f65e6378b2cae82cc949b8a5c957284
yohom <382146139@qq.com>
2020-07-15 08:43:26 +0800

    enhance: 优化混淆类识别

--- src/main/kotlin/me/yohom/fluttify/extensions/StringX.kt
+++ src/main/kotlin/me/yohom/fluttify/extensions/StringX.kt
@@ -322,7 +322,7 @@ fun TYPE_NAME.isObfuscated(): Boolean {
     val types = genericTypes()
         .map { it.replace("$", ".").substringAfterLast(".") }
         .union(listOf(containerType().replace("$", ".").substringAfterLast(".")))
-    val regex = Regex("[a-z|\\d]{0,2}")
+    val regex = Regex("[a-zA-Z|\\d]{0,2}|int|do|new|if|for")
     // objc的id类型不作为混淆类型, 如果java有个类叫id也没关系, 因为肯定会有包名在前面
     return this !in ext.obfuscatedWhiteList
             &&
