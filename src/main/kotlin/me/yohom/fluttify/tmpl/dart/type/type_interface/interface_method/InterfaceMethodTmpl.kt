f30a452771a930e5b49c5fc5bb90a808ba9592c1
yohom <382146139@qq.com>
2020-09-05 21:05:01 +0800

    enhance: feature/FluttifyMessageCodec +5

--- src/main/kotlin/me/yohom/fluttify/tmpl/dart/type/type_interface/interface_method/InterfaceMethodTmpl.kt
+++ src/main/kotlin/me/yohom/fluttify/tmpl/dart/type/type_interface/interface_method/InterfaceMethodTmpl.kt
@@ -5,8 +5,6 @@ import me.yohom.fluttify.model.Method
 
 //@mustCallSuper
 //Future<#__return_type__#> #__interface_method__#(#__formal_params__#) {
-//  #__callback_pool__#
-//
 //  if (fluttifyLogEnabled) {
 //    debugPrint('#__interface_method__#::kNativeObjectPool: $kNativeObjectPool');
 //  }
@@ -17,25 +15,9 @@ fun InterfaceMethodTmpl(method: Method): String {
     val returnType = method.returnType.toDartType()
     val name = method.signature
     val formalParams = method.formalParams.joinToString { it.variable.toDartString() }
-    val callbackPool = if (method.className.findType().isCallback) {
-        // 只有回调类的参数需要加入释放池
-        method
-            .formalParams
-            .filter { it.variable.run { !jsonable() && !isEnum() && !isAliasType() || this.trueType in method.className.findType().declaredGenericTypes } }
-            .joinToString("\n") {
-                if (it.variable.isCollection()) {
-                    "kNativeObjectPool.addAll(${it.variable.name});"
-                } else {
-                    "if (${it.variable.name} is Ref) kNativeObjectPool.add(${it.variable.name});"
-                }
-            }
-    } else {
-        ""
-    }
 
     return tmpl
         .replace("#__return_type__#", returnType)
         .replace("#__interface_method__#", name)
         .replace("#__formal_params__#", formalParams)
-        .replaceParagraph("#__callback_pool__#", callbackPool)
 }
--- src/main/kotlin/me/yohom/fluttify/tmpl/objc/common/handler/common/ref/ref_ref/RefRefTmpl.kt
+++ src/main/kotlin/me/yohom/fluttify/tmpl/objc/common/handler/common/ref/ref_ref/RefRefTmpl.kt
@@ -5,8 +5,9 @@ import me.yohom.fluttify.extensions.findType
 import me.yohom.fluttify.extensions.getResource
 import me.yohom.fluttify.model.Method
 
-//#__type_name__# ref = (#__type_name__#) HEAP[(NSNumber*) ((NSDictionary<NSString*, NSObject*>*) args)[@"refId"]];
+//#__type_name__# ref = args;
 private val tmpl by lazy { getResource("/tmpl/objc/ref_ref.stmt.m.tmpl").readText() }
+
 fun RefRefTmpl(method: Method): String {
     val empty = ""
     val protocol = "id<${method.className}>"
