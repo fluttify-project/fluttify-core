2ded8c560d4f6f03f2b534c1f3ff717e69be0626
yohom <382146139@qq.com>
2020-09-14 09:12:10 +0800

    enhance: 允许使用者区分alloc初始化和init初始化

--- src/main/kotlin/me/yohom/fluttify/tmpl/dart/type/type_sdk/creator/CreatorTmpl.kt
+++ src/main/kotlin/me/yohom/fluttify/tmpl/dart/type/type_sdk/creator/CreatorTmpl.kt
@@ -5,8 +5,8 @@ import me.yohom.fluttify.extensions.*
 import me.yohom.fluttify.model.Platform
 import me.yohom.fluttify.model.Type
 
-//static Future<#__class_name__#> create__#__signature__#(#__formal_params__#) async {
-//  final refId = await MethodChannel('#__channel_name__#', StandardMethodCodec(FluttifyMessageCodec('#__tag__#'))).invokeMethod('ObjectFactory::create#__creator_name__#'#__separator__##__args__#);
+//static Future<#__class_name__#> create__#__signature__#(#__formal_params__#{ bool init = true /* ios only */ }) async {
+//  final refId = await MethodChannel('#__channel_name__#', StandardMethodCodec(FluttifyMessageCodec('#__tag__#'))).invokeMethod('ObjectFactory::create#__creator_name__#', #__args__#);
 //  final object = #__class_name__#()..refId = refId..tag__ = '#__tag__#';
 //  return object;
 //}
@@ -32,7 +32,6 @@ fun CreatorTmpl(type: Type): List<String> {
                     .replace("#__creator_name__#", it.creatorName(type.name))
                     .replace("#__formal_params__#", it.formalParams.joinToString { it.variable.toDartString() })
                     .replace("#__channel_name__#", ext.methodChannelName)
-                    .replace("#__separator__#", if (it.formalParams.isEmpty()) "" else ", ")
                     .replace("#__args__#", it.formalParams.map { it.variable }.toDartMap())
                     .replace("#__tag__#", ext.projectName)
             }
@@ -41,10 +40,9 @@ fun CreatorTmpl(type: Type): List<String> {
                 .replace("#__class_name__#", type.name.toUnderscore())
                 .replace("#__signature__#", "")
                 .replace("#__creator_name__#", type.name.toUnderscore())
-                .replace("#__formal_params__#", "")
+                .replace("#__formal_params__#", "{ bool init = true /* ios only */ }")
                 .replace("#__channel_name__#", ext.methodChannelName)
-                .replace("#__separator__#", "")
-                .replace("#__args__#", "")
+                .replace("#__args__#", "{'init': init}")
                 .replace("#__tag__#", ext.projectName)
         )
         else -> listOf()
