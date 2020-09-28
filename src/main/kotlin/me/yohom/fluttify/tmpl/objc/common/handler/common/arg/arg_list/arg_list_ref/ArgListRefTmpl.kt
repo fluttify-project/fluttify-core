1ddcb79865a450325bddd588d74b8cfc3422b69b
yohom <382146139@qq.com>
2020-09-28 21:46:04 +0800

    enhance: NSNull转为nil

--- src/main/kotlin/me/yohom/fluttify/tmpl/objc/common/handler/common/arg/arg_list/arg_list_ref/ArgListRefTmpl.kt
+++ src/main/kotlin/me/yohom/fluttify/tmpl/objc/common/handler/common/arg/arg_list/arg_list_ref/ArgListRefTmpl.kt
@@ -4,12 +4,7 @@ import me.yohom.fluttify.extensions.*
 import me.yohom.fluttify.model.Variable
 
 //// list arg
-//NSArray<NSNumber*>* #__arg_name__#RefArray = (NSArray<NSNumber*> *) args[@"#__arg_name__#"];
-//NSMutableArray<#__type_name__#>* #__arg_name__# = [NSMutableArray arrayWithCapacity:#__arg_name__#RefArray.count];
-//for (int __i__ = 0; __i__ < #__arg_name__#RefArray.count; __i__++) {
-//    #__type_name__# item = (#__type_name__#) HEAP[[#__arg_name__#RefArray objectAtIndex:__i__]];
-//    [#__arg_name__# addObject:item];
-//}
+//NSArray<#__type_name__#>* #__arg_name__# = (NSArray<#__type_name__#>*) args[@"#__arg_name__#"];
 private val tmpl by lazy { getResource("/tmpl/objc/arg_list_ref.stmt.m.tmpl").readText() }
 
 fun ArgListRefTmpl(variable: Variable): String {
