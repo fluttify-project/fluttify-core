934c99e8b5c9d92cba640db9617068833a57c032
yohom <382146139@qq.com>
2020-10-09 21:08:34 +0800

    enhance: 优化subhandler数量判断

--- src/test/kotlin/me/yohom/fluttify/tmpl/java/plugin/JavaPluginTmplKtTest.kt
+++ src/test/kotlin/me/yohom/fluttify/tmpl/java/plugin/JavaPluginTmplKtTest.kt
@@ -10,6 +10,6 @@ internal class JavaPluginTmplKtTest : FluttifyTest() {
 
     @Test
     fun javaPluginTmpl() {
-        println(JavaPluginTmpl(Amap.androidMapSDK.directLibs, "xxx"))
+        println(JavaPluginTmpl(Amap.androidMapSDK.directLibs, "/Users/yohom/Github/Me/All/fluttify/amap/amap_map_fluttify/output-project/amap_map_fluttify/android/src/main/java/me/yohom/amap_map_fluttify/sub_handler"))
     }
 }
