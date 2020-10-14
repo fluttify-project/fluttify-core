dd20c3d13c48202e2b18f5e724f355b0467703f1
yohom <382146139@qq.com>
2020-10-14 22:15:15 +0800

    enhance: 调整

--- src/main/kotlin/me/yohom/fluttify/tmpl/dart/type/type_sdk/common/result/ResultListTmpl.kt
+++ src/main/kotlin/me/yohom/fluttify/tmpl/dart/type/type_sdk/common/result/ResultListTmpl.kt
@@ -4,7 +4,7 @@ import me.yohom.fluttify.TYPE_NAME
 import me.yohom.fluttify.ext
 import me.yohom.fluttify.extensions.*
 
-//(__result__ as List).cast<String>().map((__it__) => #__type_name__#()..refId = __it__).toList()
+//(__result__ as List)?.cast<Ref>()?.map((__it__) => __it__ == null ? null : (#__type_name__#()..refId = (__it__ as Ref).refId))?.toList()
 private val tmpl by lazy { getResource("/tmpl/dart/result_list.stmt.dart.tmpl").readText() }
 
 fun ResultListTmpl(genericType: TYPE_NAME): String {
