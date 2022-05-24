03be3aeefb565db3e1416905ae2e5699b3a952bc
yohom <yohombao@qq.com>
2022-05-24 09:47:28 +0800

    enhance: 顶层变量支持多种类型

--- src/main/kotlin/me/yohom/fluttify/tmpl/objc/common/handler/common/result/ResultJsonableTmpl.kt
+++ src/main/kotlin/me/yohom/fluttify/tmpl/objc/common/handler/common/result/ResultJsonableTmpl.kt
@@ -4,7 +4,7 @@ import me.yohom.fluttify.extensions.getResource
 
 // TODO 先运行一段时间, 看看直接用id接收会不会有什么问题
 //// 返回值: jsonable
-//id jsonableResult = result;
+//id __result__ = result;
 private val tmpl by lazy { getResource("/tmpl/objc/result_jsonable.stmt.m.tmpl").readText() }
 
 fun ResultJsonableTmpl(): String {
--- src/main/kotlin/me/yohom/fluttify/tmpl/objc/common/handler/handler_top_constant/HandlerTopConstantTmpl.kt
+++ src/main/kotlin/me/yohom/fluttify/tmpl/objc/common/handler/handler_top_constant/HandlerTopConstantTmpl.kt
@@ -1,13 +1,29 @@
 package me.yohom.fluttify.tmpl.objc.common.handler.handler_top_constant
 
-import me.yohom.fluttify.extensions.getResource
+import me.yohom.fluttify.extensions.*
 import me.yohom.fluttify.model.Variable
+import me.yohom.fluttify.tmpl.objc.common.handler.common.result.*
 
 //// top constant
 //@"get#__name__#": ^(NSObject <FlutterPluginRegistrar> * registrar, id args, FlutterResult methodResult) {
-//    methodResult.success(#__name__#);
+//    methodResult(#__name__#);
 //},
 private val tmpl by lazy { getResource("/tmpl/objc/handler_top_constant.stmt.m.tmpl").readText() }
+
 fun HandlerTopConstantTmpl(variable: Variable): String {
-    return tmpl.replace("#__name__#", variable.name)
+    val result = variable.rawType.run {
+        when {
+            isValueType() -> ResultValueTmpl()
+            jsonable() -> ResultJsonableTmpl()
+            findType().isStruct -> ResultStructTmpl(this)
+            isVoid() -> ResultVoidTmpl()
+            isPrimitivePointerType() -> ResultValuePointerTmpl()
+            else -> ResultRefTmpl()
+        }
+    }.replace("result", variable.name)
+
+    return tmpl
+        .replace("#__name__#", variable.name)
+        .replaceParagraph("#__result__#", result)
+        .replace("__result__", "__${variable.name}__") // 这里由于复用了通用的结果模板, 那边把结果变量名固定成result了, 这里没办法, 突破惯例处理一次
 }
