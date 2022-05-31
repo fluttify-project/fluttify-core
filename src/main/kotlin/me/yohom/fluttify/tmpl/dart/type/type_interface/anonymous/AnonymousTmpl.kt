70fe29169ff403b18e005efe05879447fedb7f9d
yohom <yohombao@qq.com>
2022-05-31 22:48:03 +0800

    enhance: 匿名类支持复用同一个对象

--- src/main/kotlin/me/yohom/fluttify/tmpl/dart/type/type_interface/anonymous/AnonymousTmpl.kt
+++ src/main/kotlin/me/yohom/fluttify/tmpl/dart/type/type_interface/anonymous/AnonymousTmpl.kt
@@ -4,8 +4,8 @@ import me.yohom.fluttify.extensions.*
 import me.yohom.fluttify.model.Type
 import me.yohom.fluttify.tmpl.dart.type.type_sdk.common.callback.common.callback_case.callback_case_lambda.CallbackCaseLambdaTmpl
 
-//static Future<#__class_name__#> anonymous__(#__formal_params__#) async {
-//  final __result__ = await k#__project_prefix__#Channel.invokeMethod('#__class_name__#::createAnonymous__');
+//static Future<#__class_name__#> anonymous__() async {
+//  final __result__ = await k#__project_prefix__#Channel.invokeMethod('#__channel_prefix__#::createAnonymous__');
 //
 //  final __object__ = #__project_prefix__##__platform__#As<#__class_name__#>(__result__)!;
 //
@@ -31,21 +31,13 @@ import me.yohom.fluttify.tmpl.dart.type.type_sdk.common.callback.common.callback
 private val tmpl by lazy { getResource("/tmpl/dart/anonymous.mtd.dart.tmpl").readText() }
 
 fun AnonymousTmpl(type: Type): String {
-    val formalParams = type.methods
-        .map {
-            val returnType = it.returnType.toDartType()
-            val formalParams = it.formalParams.joinToString(", ") { it.variable.toDartString(true) }
-            "$returnType Function(${formalParams})? ${it.exactName}"
-        }
-        .joinToStringX(", ", "{", "}")
     val callbackCases = type.methods
         .filterNot { it.formalParams.any { it.variable.isLambda() } }
-        .map { CallbackCaseLambdaTmpl(it, "${it.exactName}?.call") }
+        .map { CallbackCaseLambdaTmpl(it, "__object__.${it.signature}?.call") }
         .joinToStringX("\n")
     return tmpl
         .replace("#__class_name__#", type.name.toDartType())
         .replace("#__channel_prefix__#", type.name.replace("$", "."))
-        .replace("#__formal_params__#", formalParams)
         .replaceParagraph("#__cases__#", callbackCases)
         .replaceGlobal(platform = type.platform)
 }
