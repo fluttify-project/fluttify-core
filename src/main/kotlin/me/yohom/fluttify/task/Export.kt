f3c62552451b1af15501cfdc47c68454eef322ce
yohom <yohombao@qq.com>
2022-06-14 13:28:24 +0800

    enhance: 整理

--- src/main/kotlin/me/yohom/fluttify/task/Export.kt
+++ src/main/kotlin/me/yohom/fluttify/task/Export.kt
@@ -10,36 +10,40 @@ import org.gradle.api.tasks.TaskAction
 open class Export : FluttifyTask() {
     @TaskAction
     fun process() {
-        val androidDartSrcDir = "${project.projectDir}/output-project/${ext.projectName}/lib/src/android/".file()
-        val iosDartSrcDir = "${project.projectDir}/output-project/${ext.projectName}/lib/src/ios/".file()
+        val androidDartSrcDir =
+            "${project.projectDir}/output-project/${ext.projectName}/lib/src/android/".file()
+        val iosDartSrcDir =
+            "${project.projectDir}/output-project/${ext.projectName}/lib/src/ios/".file()
 
-        val androidExportFile = "${project.projectDir}/output-project/${ext.projectName}/lib/src/android/android.export.g.dart".file()
-        val iosExportFile = "${project.projectDir}/output-project/${ext.projectName}/lib/src/ios/ios.export.g.dart".file()
+        val androidExportFile =
+            "${project.projectDir}/output-project/${ext.projectName}/lib/src/android/android.export.g.dart".file()
+        val iosExportFile =
+            "${project.projectDir}/output-project/${ext.projectName}/lib/src/ios/ios.export.g.dart".file()
 
         val androidResult = StringBuilder()
         val iosResult = StringBuilder()
 
         androidDartSrcDir.iterate("dart") {
             val relativePath = it.toRelativeString(androidExportFile).removePrefix("../")
-            if (relativePath.isNotBlank()) androidResult.appendln("export '${relativePath}';")
+            if (relativePath.isNotBlank()) androidResult.appendLine("export '${relativePath}';")
         }
         iosDartSrcDir.iterate("dart") {
             val relativePath = it.toRelativeString(iosExportFile).removePrefix("../")
-            if (relativePath.isNotBlank()) iosResult.appendln("export '${relativePath}';")
+            if (relativePath.isNotBlank()) iosResult.appendLine("export '${relativePath}';")
         }
 
         ext.pluginDependencies.forEach { (name, _) ->
-            androidResult.appendln("export 'package:$name/$name.dart';")
-            iosResult.appendln("export 'package:$name/$name.dart';")
+            androidResult.appendLine("export 'package:$name/$name.dart';")
+            iosResult.appendLine("export 'package:$name/$name.dart';")
         }
 
         ext.packageDependencies.forEach { (name, _) ->
-            androidResult.appendln("export 'package:$name/$name.dart';")
-            iosResult.appendln("export 'package:$name/$name.dart';")
+            androidResult.appendLine("export 'package:$name/$name.dart';")
+            iosResult.appendLine("export 'package:$name/$name.dart';")
         }
 
-        androidResult.appendln("export '../facade/shared.g.dart';")
-        iosResult.appendln("export '../facade/shared.g.dart';")
+        androidResult.appendLine("export '../facade/shared.g.dart';")
+        iosResult.appendLine("export '../facade/shared.g.dart';")
 
         androidExportFile.writeText(androidResult.toString())
         iosExportFile.writeText(iosResult.toString())
